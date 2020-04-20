81c08864c302acc254e1a8ee81493e3712ae6fcf
Valeshka young Sang <vyoungsang@yahoo.com>
2020-04-19 19:11:42 -0500

    it works! YESgit add -A!

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-const searchbtn = document.getElementById("searchbtn");
-const inCity = document.getElementById("inCity");
-const wContainer = document.getElementsByClassName("wContainer");
+const input = document.querySelector('.input_text');
+const main = document.querySelector('#name');
+const temp = document.querySelector('.temp');
+const desc = document.querySelector('.desc');
+const clouds = document.querySelector('.clouds');
+const button = document.querySelector('.submit');
+const humidity = document.querySelector('.hum');
+const wind = document.querySelector('.wind');
 
 
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=7ed5d03992aaf558a8212aaafb8c6489&units=imperial')
+.then(response => response.json())
+.then(data => {
+  const tempVal = data['main']['temp'];
+  const humVal = data['main']['humidity'];
+  const nameVal = data['name'];
+  const descVal = data['weather'][0]['description'];
+  const windVal = data['wind']['speed'];
 
-searchbtn.onclick = function(){
-    const city = inCity.value;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=219783c004abe3c6d456793108e2dfd1`;
-fetch(url).then(reflect => {
-reflect.json().then(json =>{
-    let info =json;
-    // console.log(info);
-    let out = formResponse(info);
-    reflectOut.innerHTML = out;
 
-});
-});
-}
+  main.innerHTML = nameVal;
+  desc.innerHTML = "Description: "+descVal;
+  temp.innerHTML = "Temp: "+tempVal + " °F";
+  input.value ="";
+  humidity.innerHTML = "Humidity: " +humVal + "%";
+  wind.innerHTML = "Wind: "+ windVal + " MPH";
 
-function formResponse(info) {
-    let con =""
-    if(info.weather.length>1){
-      for(let i = 0; i < info.weather.length; i++){
-          con += info.weather[i].main;
-          if (i != (info.weather.length -1)){
+  
 
-          }
-      }
-      } else {
-          con += data.weather[0].main;
-      }  
-    
-    let output = `<p> Temperature:${Math.floor(info.main.temp)} F<br/>
-    <p>Humidity:<p>${info.main.humidity}% <br/>
-    <p>Wind::<p> ${info.wind.deg}<br/>`;
-    return(output);
-}
+})
+
+})
 
 
 
@@ -43,24 +38,37 @@ function formResponse(info) {
 
 
 
-// $('.icon').attr('src', icon);
-// $('.weather').append(weather);
-// $('.temp').append(temp);
 
-// $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city+ "&units=imperial&appid=219783c004abe3c6d456793108e2dfd1", 
-// function(info){
-//     console.log(info);
-// let icon = "http://openweathermap.org/img/w/" + info.weather[0].icon+ ".png";
-// // console.log(icon);
 
-// let temp = Math.floor(info.main.temp);
-// let weather = info.weather[0].main;
 
-// $('.icon').attr('src', icon);
-// $('.weather').append(weather);
-// $('.temp').append(temp);
 
-// }
-// );
+
+
+// $(document).ready(function() {
+// $("#searchbtn").click(function(){
+// let city = $("#City").val();
+// let key = '7ed5d03992aaf558a8212aaafb8c6489';
+
+// $.ajax({
+//     url: 'http://api.openweathermap.org/data/2.5/weather',
+//     dataType: 'json',
+//     type: 'GET',
+//     data: {q:city, appid: key, units: 'imperial'},
+
+//     success: function(data){
+//         let weatherFun = '';
+//         $.each(data.weather, function(index, val){
+//            weatherFun +=  '<p><b>' 
+//            + data.name + "</b><img src=" + val.icon +".png></p>" 
+//            + data.main.temp 
+//            + '&deg;F ' +' |' + val.main + "," + val.description
+//         });
+//         $("displayWaether").html(weatherFun);
+
+//     }
+// });
+// });
+// });
+
 
 
